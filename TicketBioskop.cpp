3212407d837c79e67450df578579e708d21b1e1d
radityanatha <radityanatha20@gmail.com>
2024-07-13 18:42:53 +0700

    Fungsi untuk Memesan Tiket

--- TicketBioskop.cpp
+++ TicketBioskop.cpp
@@ -17,12 +17,35 @@ Film daftarFilm[MAX_FILM] = {
     {"Ipar adalah Maut ", "16:00", 100},
     {"Jurnal Risa", "19:00", 100},
     {"Despicable Me 4", "21 : 00 ", 100}}
-    
+
 void tampilkanFilm()
 {
     cout << "Daftar Film:\n";
     for (int i = 0; i < MAX_FILM; i++)
     {
         cout << i + 1 << ". " << daftarFilm[i].judul << " - " << daftarFilm[i].jadwal << " - Tiket tersisa: " << daftarFilm[i].sisaTiket << endl;
+    }
+}
+void pesanTiket()
+{
+    int pilihanFilm, jumlahTiket;
+    tampilkanFilm();
+    cout << "Pilih film yang ingin dipesan (1-" << MAX_FILM << "): ";
+    cin >> pilihanFilm;
+    cout << "Masukkan jumlah tiket yang ingin dipesan: ";
+    cin >> jumlahTiket;
+
+    if (pilihanFilm < 1 || pilihanFilm > MAX_FILM)
+    {
+        cout << "Pilihan film tidak tersedia.\n";
+    }
+    else if (jumlahTiket > daftarFilm[pilihanFilm - 1].sisaTiket)
+    {
+        cout << "Jumlah tiket tidak mencukupi.\n";
+    }
+    else
+    {
+        daftarFilm[pilihanFilm - 1].sisaTiket -= jumlahTiket;
+        cout << "Selamat, Tiket berhasil dipesan!\n";
     }
 }
